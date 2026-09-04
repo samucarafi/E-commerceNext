0c5edca6624d1164b0693d88de16c0cf89bdc779
Samuel Rafino <samucarafino@gmail.com>
2026-09-04 12:23:14 -0300

    correção import User model

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import { connectMongoDB } from "@/lib/mongodb";
-import { User } from "@/models/User";
+import User from "@/models/User";
 import { signUserToken } from "@/lib/auth-server";
 
 type GoogleUserInfo = {
